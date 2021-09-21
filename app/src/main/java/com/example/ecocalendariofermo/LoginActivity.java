0cb1cdd3dc1df212ffabac6f209fa8d7c98764da
laurapistagnesi <49392902+laurapistagnesi@users.noreply.github.com>
2021-09-21 12:35:47 +0200

    Aggiunti layout di registrazione e login

--- app/src/main/java/com/example/ecocalendariofermo/LoginActivity.java
+++ app/src/main/java/com/example/ecocalendariofermo/LoginActivity.java
@@ -1,15 +1,32 @@
 package com.example.ecocalendariofermo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        setTitle("Eco Calendario");
+        getSupportActionBar().hide();
+    }
+
+    public void viewRegisterLayout(View v){
+        CardView loginCard = (CardView) findViewById(R.id.layout_login);
+        loginCard.setVisibility(View.INVISIBLE);
+        CardView registerCard = (CardView) findViewById(R.id.activity_register);
+        registerCard.setVisibility(View.VISIBLE);
+
     }
 }
