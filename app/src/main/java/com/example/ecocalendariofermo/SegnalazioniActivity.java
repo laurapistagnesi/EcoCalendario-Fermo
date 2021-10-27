03e5d32d8095e33082d97af5497a83c69dcb8806
laurapistagnesi <49392902+laurapistagnesi@users.noreply.github.com>
2021-10-27 17:09:37 +0200

    Implementata activity per le segnalazioni

---
+++ app/src/main/java/com/example/ecocalendariofermo/SegnalazioniActivity.java
@@ -0,0 +1,77 @@
+package com.example.ecocalendariofermo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SegnalazioniActivity extends AppCompatActivity {
+
+    private EditText eSubject;
+    private EditText eMsg;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segnalazioni);
+        getSupportActionBar().setTitle("Invia Segnalazione");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#166318")));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        eSubject = (EditText)findViewById(R.id.txtSub);
+        eMsg = (EditText)findViewById(R.id.txtMsg);
+        btn = (Button)findViewById(R.id.btnSend);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+    }
+
+    private void sendEmail() {
+        String subject = eSubject.getText().toString().trim();
+        String message = eMsg.getText().toString().trim();
+
+        if (subject.isEmpty()) {
+            eSubject.setError("Inserire oggetto");
+            eSubject.requestFocus();
+            return;
+        }
+
+        if (message.isEmpty()) {
+            eMsg.setError("Inserire messaggio");
+            eMsg.requestFocus();
+            return;
+        }
+        Intent it = new Intent(Intent.ACTION_SEND);
+        it.putExtra(Intent.EXTRA_EMAIL, new String[]{"laura.pistagnesi@gmail.com"});
+        it.putExtra(Intent.EXTRA_SUBJECT, eSubject.getText().toString());
+        it.putExtra(Intent.EXTRA_TEXT, eMsg.getText());
+        it.setType("message/rfc822");
+        startActivity(Intent.createChooser(it,"Choose Mail App"));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(SegnalazioniActivity.this, CalendarActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
